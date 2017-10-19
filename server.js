a78ed5899833ff02211fbe0f72dc2a7fe561786f
Robindm80 <32535948+Robindm80@users.noreply.github.com>
2017-10-19 18:56:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http');
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-   res.sendfile('index.html');
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
 });
 server.listen(process.env.PORT);
 
